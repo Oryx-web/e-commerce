18af270bbd1032d28ba75de013c73bed695d7402
Holman Nieto <holmannieto33@gmail.com>
2025-06-28 00:17:55 -0500

    vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-const path = require('path');
-    
 module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
-  content: [
-    path.join(__dirname, "./index.html"),
-    path.join(__dirname, "./src/**/*.{js,ts,jsx,tsx}"),
-    path.join(__dirname, "./src/components/**/*.{js,ts,jsx,tsx}"),
-    path.join(__dirname, "./src/pages/**/*.{js,ts,jsx,tsx}"),
-  ]
 }
